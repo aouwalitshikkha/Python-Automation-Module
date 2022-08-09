d757714fb3b98470754e14f34b1ab8065c930cc6
Abdul Aouwal <aouwalitshikkha@gmail.com>
2022-08-10 03:00:58 +0600

    Excercise One Added

--- string/exercise/exercise-1.py
+++ string/exercise/exercise-1.py
@@ -1,2 +1,28 @@
 # Write a Python function to convert string to url ?
+# Md Abdul Aouwal to md-abdul-aouwal
+
+"""
+convert it to lower case
+replace blank space with -(dash)
+convert it to function
+"""
+
+
+def urlify(string):
+    """
+    This function will convert the string to url
+    :param string: 
+    :return: Url
+    """
+    stripped_string = string.strip()
+    lower_case = stripped_string.lower()
+    url = lower_case.replace(' ','-')
+    return url
+
+
+print(urlify('Md Abdul Aouwal '))
+
+print(urlify.__doc__)
+
+
 
