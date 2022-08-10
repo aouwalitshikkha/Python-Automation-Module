383dd90b96ba007851277939119c84bdca802a6c
Abdul Aouwal <aouwalitshikkha@gmail.com>
2022-08-10 23:47:56 +0600

    List Folder Added

---
+++ list/lessons/intro.py
@@ -0,0 +1,12 @@
+"""
+What is list ?  How to Access list Items? Change List Items ?Add Items ?
+Remove list Items , Looping through list , List Comprehension, List Sorting,
+List Reversing, Copy list Items, Join List, Different methods of List
+"""
+name = "Abdul Aouwal"
+num1 = 561
+
+name_list = ['Aouwal','Jahangir','Sony', 32, 31, 35, True , False, True]
+
+print(type(name_list))
+
